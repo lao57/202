c4e54a210b0e34b3e11b3e48767492990dbb8b6e
lao57 <144056558+lao57@users.noreply.github.com>
2025-02-16 18:29:37 -0500

    Add files via upload

---
+++ LAB 3/lab3_template/Objects/uart.d
@@ -0,0 +1,4 @@
+./objects/uart.o: UART.c UART.h \
+  C:\Users\LAO57\AppData\Local\Arm\Packs\Keil\STM32L4xx_DFP\2.6.1\Drivers\CMSIS\Device\ST\STM32L4xx\Include\stm32l476xx.h \
+  C:\Users\LAO57\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Users\LAO57\AppData\Local\Arm\Packs\Keil\STM32L4xx_DFP\2.6.1\Drivers\CMSIS\Device\ST\STM32L4xx\Include\system_stm32l4xx.h
