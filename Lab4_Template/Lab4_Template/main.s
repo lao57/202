fae6309c9b7aedd389af250779c1d828f3cbc924
lao57 <144056558+lao57@users.noreply.github.com>
2025-03-12 01:30:36 -0400

    Add files via upload
    all done

--- Lab4_Template/Lab4_Template/main.s
+++ Lab4_Template/Lab4_Template/main.s
@@ -32,7 +32,7 @@ __main	PROC
 
 
 
-;;;;;;;;;;;; YOUR CODE GOES HERE	;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;; YOUR CODE GOES HERE	;;;;;;;;;;;;;;;;;;; GPIOC does rows with pins C0123, GPIOB does columns with pins B123
 
 
 ;;;;;;;;;;;;;__INIT__GPIO-C-B-CLOCK__;;;;;;;;;;;;;;;
@@ -45,8 +45,7 @@ __main	PROC
 
       LDR   r0, =GPIOB_BASE ; Configuring MODER of GPIOB for INPUTS
       LDR   r1, [r0, #GPIO_MODER] ; Loading MODER of GPIOB onto r1
-      MOV   r2, #0x00000CFC ; putting mask into r2 since BIC can't take directly
-      BIC   r1, r2  ; clearing pins 5321 to 00 (dig input)
+      BIC   r1, #0x000000FC  ; clearing pins 321 to 00 (dig input)
       STR   r1, [r0, #GPIO_MODER] ; storing change back to MODER B
       
 
@@ -60,33 +59,217 @@ __main	PROC
 ;setting columns to 1111 and rows to 0000
 
 ;Setting GPIOC to pull down
-nothing_pressed_top_of_logic
+NPTOL
+	  MOV	R4, #0; THIS VALUE HOLDS THE ROW 1-4 STARTS AT 0
 	  LDR	r0, =GPIOC_BASE
 	  LDR	r1, [r0, #GPIO_ODR]; loading in GPIOC ODR into r1
-	  BIC	r1, 0x0000000F; clearing pins C0123 for pull down
+	  BIC	r1, #0x0000000F; clearing pins C0123 for pull down
 	  STR	r1, [r0, #GPIO_ODR];sending back to GPIOC ODR
 	  
 	  BL delay; delaying
 	  
 	  LDR	r0, =GPIOB_BASE
 	  LDR	r1, [r0, #GPIO_IDR]
-	  CMP	r1, 0x0000002E; checks if pins 1235 are pressed ie != 1
-	  BEQ	nothing_pressed_top_of_logic
+	  CMP	r1, #0x0000001E; checks if pins 123 are pressed ie != 1
+	  BEQ	NPTOL
 	  
 	  BL delay;
 	  
+;Some key was pressed
+button_find
+
+	  ;CHECKING ROW 1 TO SEE IF BUTTON WAS PRESSED THERE
+      LDR r0, =GPIOC_BASE ; Configuring the ODR resgister in GPIOC for OUTPUTS
+      LDR r1, [r0, #GPIO_ODR] ; loading in GPIOC ODR into r1
+      BIC r1, #0x0000000F ; Masking the registers that we are interested in
+      ORR r1, #0x0000000E ; Setting the first bit to 0 to pull first row low
+      STR r1, [r0, #GPIO_ODR] ; sending back to GPIOC ODR
+      
+      BL delay  ; Branching to delay function as per the flowchart
+      
+
+      LDR r0, =GPIOB_BASE ; loading IDR resgister in GPIOB for INPUTS
+      LDR r2, [r0, #GPIO_IDR]  ; Loading the current value of IDR into r2
+      CMP r2, #0x0000001E ; Comparing the value if nothing is pressed
+	  MOVNE R4, #0x00000001; SETTING ROW TO ONE
+      BNE key_press ; If not equal, then a key is pressed IN ROW 1 otherwise pull down row 2
+	  
+	  ;CHECKING ROW 2 TO SEE IF BUTTON WAS PRESSED THERE
+      LDR r0, =GPIOC_BASE ; Configuring the ODR resgister in GPIOC for OUTPUTS
+      LDR r1, [r0, #GPIO_ODR] ; loading in GPIOC ODR into r1
+      BIC r1, #0x0000000F ; Masking the registers that we are interested in
+      ORR r1, #0x0000000D ; Setting the SECOND bit to 0 to pull SECOND row low
+      STR r1, [r0, #GPIO_ODR] ; sending back to GPIOC ODR
+	  
+	  BL delay;
+	  
+	  LDR r0, =GPIOB_BASE ; loading IDR resgister in GPIOB for INPUTS
+      LDR r2, [r0, #GPIO_IDR]  ; Loading the current value of IDR into r2
+      CMP r2, #0x0000001E ; Comparing the value if nothing is pressed
+	  MOVNE R4, #0x00000002; SETTING ROW TO two
+      BNE key_press ; If not equal, then a key is pressed IN ROW 2 otherwise pull down row 3
+	  
+	  ;CHECKING ROW 3 TO SEE IF BUTTON WAS PRESSED THERE
+      LDR r0, =GPIOC_BASE ; Configuring the ODR resgister in GPIOC for OUTPUTS
+      LDR r1, [r0, #GPIO_ODR] ; loading in GPIOC ODR into r1
+      BIC r1, #0x0000000F ; Masking the registers that we are interested in
+      ORR r1, #0x0000000B ; Setting the third bit to 0 to pull third row low
+      STR r1, [r0, #GPIO_ODR] ; sending back to GPIOC ODR
+	  
+	  BL delay;
+	  
+	  LDR r0, =GPIOB_BASE ; loading IDR resgister in GPIOB for INPUTS
+      LDR r2, [r0, #GPIO_IDR]  ; Loading the current value of IDR into r2
+      CMP r2, #0x0000001E ; Comparing the value if nothing is pressed
+	  MOVNE R4, #0x00000003; SETTING ROW TO three
+      BNE key_press ; If not equal, then a key is pressed IN ROW 3 otherwise pull down row 4
+	  
+	  ;CHECKING ROW 4 TO SEE IF BUTTON WAS PRESSED THERE
+      LDR r0, =GPIOC_BASE ; Configuring the ODR resgister in GPIOC for OUTPUTS
+      LDR r1, [r0, #GPIO_ODR] ; loading in GPIOC ODR into r1
+      BIC r1, #0x0000000F ; Masking the registers that we are interested in
+      ORR r1, #0x00000007 ; Setting the forth bit to 0 to pull forth row low
+      STR r1, [r0, #GPIO_ODR] ; sending back to GPIOC ODR
+	  
+	  BL delay;
+	  
+	  LDR r0, =GPIOB_BASE ; loading IDR resgister in GPIOB for INPUTS
+      LDR r2, [r0, #GPIO_IDR]  ; Loading the current value of IDR into r2
+      CMP r2, #0x0000001E ; Comparing the value if nothing is pressed
+	  MOVNE R4, #0x00000004; SETTING ROW TO four
+      BNE key_press ; If not equal, then a key is pressed IN ROW 4 otherwise go back to the start
+	  
+	  B NPTOL
+	  
+
 	  
 	  
-	
+;function for if a key was pressed
+key_press; R1 = GPIOC OUTPUT PINS 0123, R2 = GPIOB INPUT PINS 123
+	  
+	  LDR	r0, =GPIOC_BASE
+	  LDR	r1, [r0, #GPIO_ODR]; loading in GPIOC ODR into r1
+	  BIC	r1, #0x0000000F; clearing pins C0123 for pull down
+	  STR	r1, [r0, #GPIO_ODR];sending back to GPIOC ODR
+	  
+	  BL delay;
+	  
+	  LDR r0, =GPIOB_BASE ; loading IDR resgister in GPIOB for INPUTS
+      LDR r2, [r0, #GPIO_IDR]  ; Loading the current value of IDR into r2
+	  
+	  MOV r5, #0;COLUMN HOLDER
+	  CMP r2, #0x00000016; nothing pressed 0110 MEANING COLUMN 3
+	  MOVEQ R5, #0x00000030;
+	  CMP R2, #0x0000001A; 1010 column 2
+	  MOVEQ r5, #0x00000020
+	  CMP R2, #0x0000001C; just check if not in column 2 or 3
+	  MOVEQ r5, #0x00000010; this means column one
+	  
+	  BL wait_until_not_pressed
+waited  
+	  ORR r5, r4;
+	  
+	  ;COMPARES AND LOADS REGISTERS COLUMN ROW
+	  CMP r5, #0x00000011;CR
+	  BEQ L1
+	  CMP r5, #0x00000012;C = 1, R = 2...
+	  BEQ L4
+	  CMP r5, #0x00000013
+	  BEQ L7
+	  CMP r5, #0x00000014
+	  BEQ La
+	  CMP r5, #0x00000021
+	  BEQ L2
+	  CMP r5, #0x00000022
+	  BEQ L5
+	  CMP r5, #0x00000023
+	  BEQ L8
+	  CMP r5, #0x00000024
+	  BEQ L0
+	  CMP r5, #0x00000031
+	  BEQ L3
+	  CMP r5, #0x00000032
+	  BEQ L6
+	  CMP r5, #0x00000033
+	  BEQ L9
+	  CMP r5, #0x00000034
+	  BEQ Lp
+	  
+compared                         
+      BL displaykey
+	  BAL NPTOL
+
+	  
+;FUNCTIONS FOR LOADING VALUES
+;loads value from mem to r7
+;assigns ascii value to r6 
+;not sure which methoud is needed
+L0	LDR r7, =zero
+	MOV r6, #048
+	BAL compared
+L1	LDR r7, =one
+	MOV r6, #049
+	BAL compared
+L2 	LDR r7, =two
+	MOV r6, #050
+	BAL compared
+L3	LDR r7, =three
+	MOV r6, #051
+	BAL compared
+L4	LDR r7, =four
+	MOV r6, #052
+	BAL compared
+L5	LDR r7, =five
+	MOV r6, #053
+	BAL compared
+L6	LDR r7, =six
+	MOV r6, #054
+	BAL compared
+L7	LDR r7, =seven
+	MOV r6, #055
+	BAL compared
+L8	LDR r7, =eight
+	MOV r6, #056
+	BAL compared
+L9	LDR r7, =nine
+	MOV r6, #057
+	BAL compared
+La	LDR r7, =astersik
+	MOV r6, #042
+	BAL compared
+Lp	LDR r7, =Pound
+	MOV r6, #035
+	BAL compared
+
+	  
+	  
+	  
+wait_until_not_pressed;loop according to diagram
+	  LDR	r0, =GPIOC_BASE
+	  LDR	r1, [r0, #GPIO_ODR]; loading in GPIOC ODR into r1
+	  BIC	r1, #0x0000000F; clearing pins C0123 for pull down
+	  STR	r1, [r0, #GPIO_ODR];sending back to GPIOC ODR
+	  
+	  BL delay; delaying
+	  
+	  LDR	r0, =GPIOB_BASE
+	  LDR	r1, [r0, #GPIO_IDR]
+	  CMP	r1, #0x0000001E; checks if pins 123 are pressed ie != 1
+	  BNE	wait_until_not_pressed
+	  BAL	waited
 
 	
 displaykey
-	STR	r5, [r8]
-	LDR	r0, =char1
+	;STR	r6, [r8]
+	;MOV	r0, r7
 	;LDR r0, =str   ; First argument
-	MOV r1, #1    ; Second argument
+	;MOV r1, #1    ; Second argument
+	
+	STR r6, [r8]  ; Goes onto given display function if the button is not pressed
+    MOV r0, r7
+    MOV r1, #1   ; Second arugment
 	BL USART2_Write
- 	
+ 	BAL NPTOL
 	ENDP		
 
 			
@@ -111,5 +294,18 @@ delayloop
 	AREA myData, DATA, READWRITE
 	ALIGN
 
-char1	DCD	43
+; Creating memory space for the corresponding ASCII values of 0-9, A-D, *, and #. 
+non DCD 48
+zero DCD 49
+one DCD 50
+two DCD 51
+three DCD 52
+four DCD 53
+five DCD 54
+six DCD 55
+seven DCD 56
+eight DCD 57
+nine DCD 42
+astersik DCD 35
+Pound DCD 35
 	END
