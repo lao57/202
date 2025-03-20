989c3b6b9e2c81f46a499a54f0fab99a80845e7e
lao57 <144056558+lao57@users.noreply.github.com>
2025-03-20 13:58:19 -0400

    Delete Lab 5_Template/Lab 5_Template directory

--- Lab 5_Template/Lab 5_Template/Objects/uart.d
+++
@@ -1,4 +0,0 @@
-./objects/uart.o: UART.c UART.h \
-  C:\Users\Owner\AppData\Local\Arm\Packs\Keil\STM32L4xx_DFP\2.6.1\Drivers\CMSIS\Device\ST\STM32L4xx\Include\stm32l476xx.h \
-  C:\Users\Owner\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\core_cm4.h \
-  C:\Users\Owner\AppData\Local\Arm\Packs\Keil\STM32L4xx_DFP\2.6.1\Drivers\CMSIS\Device\ST\STM32L4xx\Include\system_stm32l4xx.h
